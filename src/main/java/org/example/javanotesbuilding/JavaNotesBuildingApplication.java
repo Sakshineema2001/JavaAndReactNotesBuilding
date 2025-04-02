ad0f4c4ff67a0bc9e047b12390ba5ade239a0627
Sakshi Neema <S@123neema>
2025-04-02 11:16:35 +0530

    Initial commit

---
+++ src/main/java/org/example/javanotesbuilding/JavaNotesBuildingApplication.java
@@ -0,0 +1,13 @@
+package org.example.javanotesbuilding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaNotesBuildingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaNotesBuildingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/javanotesbuilding/JavaNotesBuildingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.javanotesbuilding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaNotesBuildingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
